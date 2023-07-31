9cf99f13fd249d0d04f870bf08dab9dc45ddbfd3
Taylor Southwick <tasou@microsoft.com>
2023-07-31 10:17:38 -0700

    add default implementation

--- src/Hosting/Abstractions/src/IWebHostEnvironment.cs
+++ src/Hosting/Abstractions/src/IWebHostEnvironment.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections;
+using System.Linq;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
@@ -15,7 +17,7 @@ public interface IWebHostEnvironment : IHostEnvironment
     /// <summary>
     /// Gets a feature collection for the hosting environment.
     /// </summary>
-    IFeatureCollection Features { get; }
+    IFeatureCollection Features => EmptyFeatures.Instance;
 
     /// <summary>
     /// Gets or sets the absolute path to the directory that contains the web-servable application content files.
@@ -28,4 +30,27 @@ public interface IWebHostEnvironment : IHostEnvironment
     /// This defaults to referencing files from the 'wwwroot' subfolder.
     /// </summary>
     IFileProvider WebRootFileProvider { get; set; }
+}
+
+internal sealed class EmptyFeatures : IFeatureCollection
+{
+    public static EmptyFeatures Instance { get; } = new();
+
+    public object? this[Type key]
+    {
+        get => null;
+        set => throw new NotImplementedException();
+    }
+
+    public bool IsReadOnly => true;
+
+    public int Revision => 0;
+
+    public TFeature? Get<TFeature>() => default;
+
+    public IEnumerator<KeyValuePair<Type, object>> GetEnumerator() => Enumerable.Empty<KeyValuePair<Type, object>>().GetEnumerator();
+
+    public void Set<TFeature>(TFeature? instance) => throw new NotImplementedException();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
